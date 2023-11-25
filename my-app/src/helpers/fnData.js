8f92d03c63f80a5a7bc090d730c07584ddd23801
SergeyZhydkov <134008114+SergeyZhydkov@users.noreply.github.com>
2023-11-25 13:05:35 +0200

    add files

--- my-app/src/helpers/fnData.js
+++ my-app/src/helpers/fnData.js
@@ -1,7 +1,8 @@
-import { formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNow, format } from 'date-fns';
 
-
-
-export const onCreateData = (newData) => { 
-    return formatDistanceToNow(new Date(newData), { addSuffix: true })
-} 
+export const onCreateData = newData => {
+  return formatDistanceToNow(new Date(newData), { addSuffix: true });
+};
+export const onFormatData = newData => {
+  return format(new Date(newData), 'Pp');
+};
