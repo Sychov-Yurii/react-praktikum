b45760327b6df0fa83883657dc3e4e6698cdacca
AlexKat <katsubo79@gmail.com>
2023-11-25 12:34:58 +0200

    3task+

--- my-app/src/components/ForbesList/ForbesList.jsx
+++ my-app/src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,30 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({forbes}) => {
+  return   (
+      <LeaderBoard>
+  <BoardHeader>
+    <BoardTitle>
+      <TitleTop>Forbes</TitleTop>
+      <TitleBottom>Leader board</TitleBottom>
+    </BoardTitle>
+  </BoardHeader>
+
+  <LeaderBoardProfiles>
+        {forbes.map(({
+          avatar,
+          name,
+          isIncrease,
+          capital }) => (
+          < ForbesListItem
+            avatar={avatar}
+            name={name}
+            isIncrease={isIncrease}
+            capital={capital}
+          />
+        ))}
+  </LeaderBoardProfiles>
+</LeaderBoard>
+    )
 };
--- my-app/src/components/ForbesListItem/ForbesListItem.jsx
+++ my-app/src/components/ForbesListItem/ForbesListItem.jsx
@@ -4,6 +4,17 @@ import { ForbesItem, Avatar, Name, Capital } from './ForbesListItem.styled';
 
 import { theme } from 'styles/theme';
 
-export const ForbesListItem = () => {
-  return <div>ForbesItem</div>;
+export const ForbesListItem = ({capital, name, avatar, isIncrease}) => {
+  return (
+    <ForbesItem>
+  <Avatar
+    src={avatar}
+  />
+  <Name>{name}</Name>
+  <Capital>
+   {capital} <BiDollarCircle color={theme.colors.accent} size={22} />
+    {isIncrease ? <FcBullish/> : <FcBearish/>}
+  </Capital>
+</ForbesItem>
+  )
 };
