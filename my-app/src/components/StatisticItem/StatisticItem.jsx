f683fdd5131123f3a21d37b9bcff3bd399f7fb35
sunny7657 <ksvetlana466@gmail.com>
2023-11-25 11:01:05 +0100

    upd

--- my-app/src/components/StatisticItem/StatisticItem.jsx
+++ my-app/src/components/StatisticItem/StatisticItem.jsx
@@ -3,7 +3,16 @@ import {
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
+import { IconContext } from 'react-icons';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ title, total, icon }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ size: '48px' }}>
+        {icon}
+      </IconContext.Provider>
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
 };
